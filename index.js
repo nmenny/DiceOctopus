77c8c4c418dbd419d5e8394b91b8b69d6f4d7d37
Nathan <nathanmenny@gmail.com>
2024-02-25 09:09:04 +0100

    setup bot

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const { Client, Events, GatewayIntentBits } = require("discord.js");
 const dotenv = require("dotenv");
 
+
+// Get the configurations
 dotenv.config();
 
+// Setup Discord Bot
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.login(process.env.TOKEN);
