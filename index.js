9017ff4fa1342d362780e8263c7398d1cebd4e12
Nathan <nathanmenny@gmail.com>
2024-02-25 09:33:35 +0100

    add code to get all the commands

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
+
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
 const dotenv = require("dotenv");
 
 
@@ -8,4 +11,34 @@ dotenv.config();
 // Setup Discord Bot
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.login(process.env.TOKEN);
+// Gets the commands
+client.commands = new Collection();
+
+const commandFolderPath = path.join(__dirname, "commands");
+
+function addCommand(currPath) {
+    const command = require(currPath);
+
+    if("data" in command && "execute" in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[WARNING] The command at ${currPath} is missing a required "data" or "execute" property.`);
+    }
+}
+
+function exploreCommandsFolder(currPath, fileName = "") {
+    if(fs.lstatSync(currPath).isFile() && fileName.endsWith(".js")) {
+        addCommand(currPath);
+    } else {
+        const folder = fs.readdirSync(currPath);
+
+        for(const content of folder) {
+            const commandPath = path.join(currPath, content);
+            exploreCommandsFolder(commandPath, content);
+        }
+    }
+}
+
+exploreCommandsFolder(commandFolderPath);
+
+// client.login(process.env.TOKEN);
