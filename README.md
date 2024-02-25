b37c7691408fb839d22eb9226545a44aa0c98275
Nathan <nathanmenny@gmail.com>
2024-02-25 10:23:58 +0100

    add script to deploy the commands to the servers

--- README.md
+++ README.md
@@ -22,11 +22,24 @@ To setup the bot, you must define a `.env` file containing the following variabl
 
 ```txt
 TOKEN=<token of your Discord bot>
+CLIENT_ID=<id of the application>
+GUILD_ID=<id of the development guild> (mandatory if ENV is not "deployment")
+ENV=<"development" or "deployment"> (default is "deployment")
 ```
+
+For the `ENV` variable, if its value is "development" the commands are only updated in the Discord Server with the id `GUILD_ID` otherwise the commands are updated in all the Discord Servers where the bot can execute / (SLASH) commands.
+
+The `GUILD_ID` and `ENV` must be defined only if you want to deploy in "development" mode.
 
 ### Start
 
-To start the Bot, run the following command :
+Firstly, the commands must be deployed in the target servers. To do so, execute the script `deploy-commands.js` using the following command :
+
+```bash
+node deploy-commands.js
+```
+
+And then, the Bot can be started with the following command :
 
 ```bash
 node .
---
+++ deploy-commands.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const path = require("path");
+
+const { REST, Routes } = require("discord.js");
+const dotenv = require("dotenv");
+
+const { exploreCommandsFolder } = require("./utils");
+
+
+// --------------
+// Get the configurations
+dotenv.config();
+const appEnv = process.env.ENV ?? "deployment";
+
+// --------------
+// Get the commands
+const commands = [];
+const commandFolderPath = path.join(__dirname, "commands");
+
+exploreCommandsFolder(commandFolderPath, "", commands);
+
+for(let cmdIdx in commands) {
+    commands[cmdIdx] = commands[cmdIdx].data.toJSON();
+}
+
+// --------------
+// REST config
+const rest = new REST().setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+        let route = undefined;
+
+        if(appEnv !== "deployment") {
+            route = Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID);
+        } else {
+            route = Routes.applicationCommands(process.env.CLIENT_ID);
+        }
+        
+        const data = await rest.put(
+            route,
+            { body: commands },
+        );
+
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (err) {
+		console.error(err);
+	}
+})();
